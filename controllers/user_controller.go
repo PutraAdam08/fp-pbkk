5ef9389e7eece7551ecaa9c5688e5d99d12de62f
PutraAdam08 <putraadam322@gmail.com>
2024-11-30 16:46:45 +0700

    Add user_controller.go

---
+++ controllers/user_controller.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"fp_pbkk/models"
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/render"
+)
+
+type userData struct {
+	Email    string `form:"email"`
+	Password string `form:"password"`
+}
+
+func signUp(c *gin.Context) {
+	var data userData
+	c.Bind(&data)
+
+	if !models.CheckUserAvailability(data.Email) {
+		c.Render(http.StatusBadRequest, render.Data{})
+		return
+	}
+
+	user := models.UserCreate(data.Email, data.Password)
+	if user == nil || user.ID == 0 {
+		c.Render(http.StatusBadRequest, render.Data{})
+		return
+	}
+
+	session := sessions.Default(c)
+	session.Set("userID", user.ID)
+	session.Save()
+	//c.Redirect(http.StatusFound, "/blogs")
+}
+
+func Login(c *gin.Context) {
+	var data userData
+	c.Bind(&data)
+
+	user := models.UserMatchPassword(data.Email, data.Password)
+	if user.ID == 0 {
+		c.Render(http.StatusUnauthorized, render.Data{})
+		return
+	}
+
+	session := sessions.Default(c)
+	session.Set("userID", user.ID)
+	session.Save()
+
+	c.Redirect(http.StatusFound, "/blogs")
+}
+
+func Logout(c *gin.Context) {
+	session := sessions.Default(c)
+	session.Clear()
+	session.Save()
+	c.Status(http.StatusAccepted)
+}
